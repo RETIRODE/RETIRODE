a222e0ae95faa16b1c8ef7c13c626a269c7a6f92
Martin <trojan@datapac.sk>
2020-10-18 16:29:01 +0200

    trying to communicate with lidar

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,23 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: uart_cmsis_driver
+ * RTE configuration: uart_cmsis_driver.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "rsl10.h"
+
+/* ONSemiconductor::CMSIS Driver.USART.USART.source */
+#define RTECFG_USART
+/* ONSemiconductor::Device.Libraries.DMA.source */
+#define RTECFG_DMA
+/* ONSemiconductor::Device.Libraries.GPIO.source */
+#define RTECFG_GPIO
+
+#endif /* RTE_COMPONENTS_H */
---
+++ include/app.h
@@ -0,0 +1,82 @@
+/* ----------------------------------------------------------------------------
+ * Copyright (c) 2019 Semiconductor Components Industries, LLC (d/b/a
+ * ON Semiconductor), All Rights Reserved
+ *
+ * This code is the property of ON Semiconductor and may not be redistributed
+ * in any form without prior written permission from ON Semiconductor.
+ * The terms of use and warranty for this code are covered by contractual
+ * agreements between ON Semiconductor and the licensee.
+ *
+ * This is Reusable Code.
+ * ----------------------------------------------------------------------------
+ * app.h
+ * - Application header file for the uart_cmsis_driver sample
+ *   application
+ * ------------------------------------------------------------------------- */
+
+#ifndef APP_H
+#define APP_H
+
+/* ----------------------------------------------------------------------------
+ * If building with a C++ compiler, make all of the definitions in this header
+ * have a C binding.
+ * ------------------------------------------------------------------------- */
+#ifdef __cplusplus
+extern "C"
+{
+#endif    /* ifdef __cplusplus */
+
+/* ----------------------------------------------------------------------------
+ * Include files
+ * --------------------------------------------------------------------------*/
+
+#include <RTE_Device.h>
+#include <GPIO_RSLxx.h>
+#include <rsl10.h>
+#include <string.h>
+#include <stdio.h>
+#include <USART_RSLxx.h>
+#include "app_trace.h"
+
+
+/* ----------------------------------------------------------------------------
+ * Defines
+ * --------------------------------------------------------------------------*/
+#if !RTE_USART
+    #error "Please configure USART0 in RTE_Device.h"
+#endif    /* if !RTE_USART0 */
+
+
+/* DIO number that is used for easy re-flashing (recovery mode) */
+#define RECOVERY_DIO                    12
+
+/* GPIO definitions */
+#define LED_DIO                         6
+#define BUTTON_DIO                      5
+
+/* Buffer offset */
+#define BUFFER_OFFSET                   5
+
+/* ----------------------------------------------------------------------------
+ * Global variables and types
+ * --------------------------------------------------------------------------*/
+extern ARM_DRIVER_USART Driver_USART0;
+extern DRIVER_GPIO_t Driver_GPIO;
+#define TEST_QUERY						"R??\r"
+
+/* ---------------------------------------------------------------------------
+* Function prototype definitions
+* --------------------------------------------------------------------------*/
+void Usart_EventCallBack(uint32_t event);
+void Button_EventCallback(uint32_t event);
+void Initialize(void);
+void ToggleLed(uint32_t n, uint32_t delay_ms);
+
+/* ----------------------------------------------------------------------------
+ * Close the 'extern "C"' block
+ * ------------------------------------------------------------------------- */
+#ifdef __cplusplus
+}
+#endif    /* ifdef __cplusplus */
+
+#endif    /* APP_H */
