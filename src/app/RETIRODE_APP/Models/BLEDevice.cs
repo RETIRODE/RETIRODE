21e77dd33f7c16fcae9ea7075210afc6073bdf9d
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-31 00:49:24 +0200

    feat: create help model of BT device
    only necessary parameters included within BT device model

---
+++ src/app/RETIRODE_APP/Models/BLEDevice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.Models
+{
+    public class BLEDevice
+    {
+        public Guid Identifier { get; set; }
+
+        public string Name { get; set; }
+    }
+
+}
