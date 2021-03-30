e50c069151d099aaa58e393ce7d0acba94eaba0a
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-30 21:35:07 +0200

    feat: create basic skeleton of BT service

---
+++ src/app/RETIRODE_APP/Services/BluetoothService.cs
@@ -0,0 +1,32 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using RETIRODE_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RETIRODE_APP.Services
+{
+    class BluetoothService : IBluetoothService
+    {
+        public bool connectToDevice(IDevice btDevice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IDevice> GetBluetoothDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ReadCharacteristic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> WriteCharacteristic()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/app/RETIRODE_APP/Services/IBluetoothService.cs
@@ -0,0 +1,20 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using RETIRODE_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RETIRODE_APP.Services
+{
+    public interface IBluetoothService
+    {
+        List<IDevice> GetBluetoothDevices();
+
+        bool connectToDevice(IDevice btDevice); 
+
+        Task<bool> WriteCharacteristic();
+
+        Task<string> ReadCharacteristic();
+    }
+}
