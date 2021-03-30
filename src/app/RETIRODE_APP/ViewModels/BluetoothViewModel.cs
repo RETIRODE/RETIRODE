84b16546a7bb38a5e0946377d9c24fd40f487da1
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-30 21:34:39 +0200

    feat: create basic skeleton of BT view model

---
+++ src/app/RETIRODE_APP/ViewModels/BluetoothViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Nancy.TinyIoc;
+using RETIRODE_APP.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.ViewModels
+{
+    class BluetoothViewModel : BaseViewModel
+    {
+        private IBluetoothService _btService => TinyIoCContainer.Current.Resolve<IBluetoothService>();
+
+        public BluetoothViewModel()
+        {
+            //sample
+            _btService.GetBluetoothDevices();
+        }
+    }
+}
