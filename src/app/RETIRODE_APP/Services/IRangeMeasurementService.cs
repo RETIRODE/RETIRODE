4e2b00eca51791249535a52a2451837076438504
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-31 00:51:54 +0200

    feat: add interlayer RangeMeasurementService
    this removes interaction between bluetooth service and view model straightforward

---
+++ src/app/RETIRODE_APP/Services/IRangeMeasurementService.cs
@@ -0,0 +1,23 @@
+﻿using RETIRODE_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RETIRODE_APP.Services
+{
+    public interface IRangeMeasurementService
+    {
+        bool StartMeasurement(RSL10Command command);
+
+        bool StopMeasurement(RSL10Command command);
+
+        Task<bool> ConnectToRSL10(BLEDevice bleDevice);
+
+        IEnumerable<BLEDevice> AvailableDevices();
+
+        bool CalibrateLIDAR(RSL10Command command);
+
+        Task<bool> DisconnectFromRSL10();
+    }
+}
---
+++ src/app/RETIRODE_APP/Services/RangeMeasurementService.cs
@@ -0,0 +1,53 @@
+﻿using Nancy.TinyIoc;
+using RETIRODE_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RETIRODE_APP.Services
+{
+    public class RangeMeasurementService : IRangeMeasurementService
+    {
+        public IEnumerable<BLEDevice> AvailableDevices()
+        {
+            // TEMPORARY MOCKED DATA
+            List<BLEDevice> devices = new List<BLEDevice>();
+            Random random = new Random();
+            for(int i = 0; i < 2; i++)
+            {
+                devices.Add(new BLEDevice()
+                {
+                    Name = "asd" + i,
+                    Identifier = Guid.NewGuid()
+                });
+            }
+            return devices;
+        }
+
+        public bool CalibrateLIDAR(RSL10Command command)
+        {
+            return true;
+        }
+
+        public Task<bool> ConnectToRSL10(BLEDevice bleDevice)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> DisconnectFromRSL10()
+        {
+            return Task.FromResult(true);
+        }
+
+        public bool StartMeasurement(RSL10Command command)
+        {
+            return true;
+        }
+
+        public bool StopMeasurement(RSL10Command command)
+        {
+            return true;
+        }
+    }
+}
