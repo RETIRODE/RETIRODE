655839b3359cf3538fddd702fb4cdf8f88004329
EfRy EfRy <jakubtrstensky@gmail.com>
2021-03-31 22:59:51 +0200

    feat: add found devices into list

--- src/app/RETIRODE_APP/Services/RangeMeasurementService.cs
+++ src/app/RETIRODE_APP/Services/RangeMeasurementService.cs
@@ -3,6 +3,8 @@ using Plugin.BLE.Abstractions.Contracts;
 using RETIRODE_APP.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,15 @@ namespace RETIRODE_APP.Services
 {
     public class RangeMeasurementService : IRangeMeasurementService
     {
+        //------------- STATIC VARIABLES -------------//
+        private static Guid GattServiceId = Guid.Parse("5177db0a-8ce6-11eb-8dcd-0242ac130003");
+        private static Guid GattCharacteristicReceiveId = Guid.Parse("");
+        private static Guid GattCharacteristicSendId = Guid.Parse("");
+        private static Guid SpecialNotificationDescriptorId = Guid.Parse("");
+        private static string RetirodeUniqueMacAddressPart = "60:C0:BF";
+        private static int UniqueMacAddressLength = 8;
+
+        //------------- CLASS VARIABLES -------------//
         private readonly IBluetoothService _bluetoothService;
         private IList<IDevice> _availableDevices;
         public IList<BLEDevice> AvailableDevices { get; private set; }
@@ -26,9 +37,9 @@ namespace RETIRODE_APP.Services
             throw new NotImplementedException();
         }
 
-        public Task<bool> ConnectToRSL10(BLEDevice bleDevice)
+        public async Task<bool> ConnectToRSL10(BLEDevice bleDevice)
         {
-            throw new NotImplementedException();
+            return await Task.FromResult(true);
         }
 
         public Task<bool> Disconnect(BLEDevice device)
@@ -53,15 +64,15 @@ namespace RETIRODE_APP.Services
             throw new NotImplementedException();
         }
 
-        private void DeviceDiscovered(object sender, IDevice device)
+        private async void DeviceDiscovered(object sender, IDevice device)
         {
-            AvailableDevices.Add(new BLEDevice
-            {
-                Name = device.Name,
-                Identifier = device.Id
-            });
+                AvailableDevices.Add(new BLEDevice
+                {
+                    Name = device.Name,
+                    Identifier = device.Id
+                });
 
-            _availableDevices.Add(device);
+                _availableDevices.Add(device);
         }
     }
 }
