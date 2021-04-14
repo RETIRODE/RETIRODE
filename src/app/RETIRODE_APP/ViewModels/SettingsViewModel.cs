cb1febfc1fe01da2b8bcf9e296a12e47d9f6d383
Simon Babic <babic.simon.97@gmail.com>
2021-04-14 22:11:03 +0200

    feat: add settings page

---
+++ src/app/RETIRODE_APP/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.ViewModels
+{
+    class SettingsViewModel : BaseViewModel
+    {
+        public SettingsViewModel()
+        {
+            Title = "Settings";
+        }
+
+        public double TCDCal0 { get; set; }
+        public double TCDCal62 { get; set; }
+        public double TCDCal125 { get; set; }
+        public double TriggerPulse { get; set; }
+        public double VoltagePulse { get; set; }
+        public double SIPMTargetV { get; set; }
+        public double SIPMActualV { get; set; }
+        public double LaserTargetV { get; set; }
+        public double LaserActualV { get; set; }
+    }
+}
