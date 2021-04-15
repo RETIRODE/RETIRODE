6f8de2763847c835cd0bdab2b4e81d294c2d14ff
EfRy EfRy <jakubtrstensky@gmail.com>
2021-04-15 03:44:47 +0200

    feat: create enums according to protocol

---
+++ src/app/RETIRODE_APP/Models/Enums/Registers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.Models.Enums
+{
+    public enum Registers : Byte
+    {
+        SWReset = 0x00,
+        LaserVoltage = 0x01,
+        SipmBiasPowerVoltage = 0x02,
+        Calibrate = 0x03
+    }
+}
