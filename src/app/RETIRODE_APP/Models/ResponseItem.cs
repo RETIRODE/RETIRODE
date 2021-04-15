f157445f2dd3c2c959fe80c0b0c455e1beb06403
EfRy EfRy <jakubtrstensky@gmail.com>
2021-04-15 03:43:45 +0200

    feat: create ResponseItem
    due to multiple response from BT, for better communication

---
+++ src/app/RETIRODE_APP/Models/ResponseItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.Models
+{
+    public class ResponseItem
+    {
+        public string Identifier { get; set; }
+
+        public string Target { get; set; }
+
+        public string Actual { get; set; }
+
+        public string Calibrate_0 { get; set; }
+
+        public string Calibrate_62_5 { get; set; }
+
+        public string Calibrate_125 { get; set; }
+ 
+    }
+}
