4f8e617b401ef49cb5a93cc2f36734b5ca33f54e
Maros Kovalak <kovalak.maros@gmail.com>
2021-04-21 21:42:45 +0200

    feat: add class which represents tables in db

---
+++ src/app/RETIRODE_APP/Models/MeasurementItem.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.Models
+{
+    public class MeasurementItem : BaseItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public CalibrationItem CalibrationItem { get; set; }
+
+        public float Tdc_value { get; set; }
+        public float Angle { get; set; }
+
+    }
+}
