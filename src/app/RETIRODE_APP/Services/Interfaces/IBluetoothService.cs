a8695427b2fb694d01e49b770119f1c6589e8b6e
japubek <jakubtrstensky@gmail.com>
2021-04-21 23:22:05 +0200

    docs: add method comments in interfaces

--- src/app/RETIRODE_APP/Services/Interfaces/IBluetoothService.cs
+++ src/app/RETIRODE_APP/Services/Interfaces/IBluetoothService.cs
@@ -7,20 +7,22 @@ namespace RETIRODE_APP.Services
     public interface IBluetoothService
     {
         /// <summary>
-        /// 
+        /// Starts asynchornous scanning for available devices
+        /// nearby
         /// </summary>
         /// <returns></returns>
         Task StartScanning();
 
         /// <summary>
-        /// 
+        /// Connect to device straightforward, also set 
+        /// the higher speed of data transport
         /// </summary>
         /// <param name="btDevice" <see cref="IDevice"/>></param>
         /// <returns></returns>
         Task ConnectToDeviceAsync(IDevice btDevice);
 
         /// <summary>
-        /// 
+        /// writing to specific characteristic
         /// </summary>
         /// <param name="characteristic" <see cref="ICharacteristic"/>></param>
         /// <param name="message"></param>
@@ -28,7 +30,7 @@ namespace RETIRODE_APP.Services
         Task<bool> WriteToCharacteristic(ICharacteristic characteristic, byte[] message);
 
         /// <summary>
-        /// 
+        /// event fired when some device nearby is found
         /// </summary>
         Action<object, IDevice> DeviceFound { get; set; }
     }
--- src/app/RETIRODE_APP/Services/Interfaces/IRangeMeasurementService.cs
+++ src/app/RETIRODE_APP/Services/Interfaces/IRangeMeasurementService.cs
@@ -10,17 +10,19 @@ namespace RETIRODE_APP.Services
     public interface IRangeMeasurementService : IDisposable
     {
         /// <summary>
-        /// 
+        /// This event is fired immadiately when device is found 
+        /// and checked by white list
         /// </summary>
         event Action<BLEDevice> DeviceDiscoveredEvent;
 
         /// <summary>
-        /// 
+        /// Event is fired when response from query characteristic
+        /// is given
         /// </summary>
         event Action<ResponseItem> QueryResponseEvent;
 
         /// <summary>
-        /// 
+        /// Event is fired when response from measuring is given
         /// </summary>
         event Action QueryDataReceivedEvent;
 
@@ -37,7 +39,10 @@ namespace RETIRODE_APP.Services
         Task StopMeasurement();
 
         /// <summary>
-        /// 
+        /// Connect to device by param which is given(tapped) from GUI,
+        /// set all available characteristics and services, device has
+        /// and start to listening to those characteristic which are in 
+        /// read (notify) state
         /// </summary>
         /// <param name="bleDevice"></param>
         /// <returns></returns>
@@ -50,20 +55,20 @@ namespace RETIRODE_APP.Services
         Task StartScanning();
 
         /// <summary>
-        /// 
+        /// set lidar to basic settings
         /// </summary>
         /// <returns></returns>
         Task SwReset();
 
         /// <summary>
-        /// 
+        /// set laser voltage value for lidar
         /// </summary>
         /// <param name="laserVoltage"></param>
         /// <returns></returns>
         Task SetLaserVoltage(int laserVoltage);
 
         /// <summary>
-        /// 
+        /// set sipm bias voltage power value for lidar
         /// </summary>
         /// <param name="sipmBiasPowerVoltage"></param>
         /// <returns></returns>
@@ -76,25 +81,28 @@ namespace RETIRODE_APP.Services
         Task CalibrateLidar();
 
         /// <summary>
-        /// 
+        /// set pulse count for lidar
         /// </summary>
         /// <returns></returns>
         Task SetPulseCount(int pulseCount);
 
         /// <summary>
-        /// 
+        /// Send request for target or actual value of lidar's
+        /// laser voltage
         /// </summary>
         /// <returns></returns>
         Task GetLaserVoltage(Voltage voltage);
 
         /// <summary>
-        /// 
+        /// Send request for target or actual value of lidar's
+        /// sipm bias voltage power
         /// </summary>
         /// <returns></returns>
         Task GetSipmBiasPowerVoltage(Voltage voltage);
 
         /// <summary>
-        /// 
+        /// Send request for one of three calibration values
+        /// of lidar
         /// </summary>
         /// <returns></returns>
         Task GetCalibration(Calibrate calibrate);
