6cc93e432f45ac01d374044eee24a96a652a64fc
japubek <jakubtrstensky@gmail.com>
2021-04-21 18:18:09 +0200

    feat: create enums

---
+++ src/app/RETIRODE_APP/Models/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.Models
+{
+    public static class Constants
+    {
+        //Gatt identifiers
+        public static readonly Guid GattFirstServiceId = Guid.Parse("5177db0a-8ce6-11eb-8dcd-0242ac130003");
+        public static readonly Guid GattFirstCharacteristicReceiveId = Guid.Parse("00000000000000000000000000000000");
+        public static readonly Guid GattFirstCharacteristicWriteId = Guid.Parse("5177de8e-8ce6-11eb-8dcd-0242ac130003");
+        public static readonly Guid GattSecondCharacteristicReceiveId = Guid.Parse("00000000000000000000000000000000");
+        public static readonly Guid GattSecondCharacteristicWriteId = Guid.Parse("00000000000000000000000000000000");
+        public static readonly Guid GattSecondServiceId = Guid.Parse("00000000000000000000000000000000");
+        public static readonly string RetirodeUniqueMacAddressPart = "60:C0:BF";
+        public static readonly string UniqueRetirodeName = "Retirode";
+        public static readonly int UniqueMacAddressLength = 8;
+    }
+}
---
+++ src/app/RETIRODE_APP/Models/Enums/RangeFinderValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.Models.Enums
+{
+    public enum RangeFinderValues
+    {
+        LaserVoltageTarget,
+        LaserVoltageActual,
+        SipmBiasPowerVoltageTarget,
+        SipmBiasPowerVoltageActual,
+        CalibrateNS0,
+        Calibrate62_5,
+        Calibrate125
+    }
+}
