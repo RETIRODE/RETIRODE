b13b337f2df81b6f11c2370db3e83632432fdd7b
japubek <jakubtrstensky@gmail.com>
2021-05-11 23:52:53 +0200

    feat: create enum for switch cases in Lidar

---
+++ src/app/RETIRODE_APP/Models/Enums/Switch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RETIRODE_APP.Models.Enums
+{
+    public enum Switch : Byte
+    {
+        TurnOff = 0x00,
+        TurnOn = 0x01
+    }
+}
