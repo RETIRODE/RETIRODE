1b3235a01214e5bef9e8e2e06264c538f91b96e9
japubek <jakubtrstensky@gmail.com>
2021-05-11 23:02:07 +0200

    feat: implement calibration lidar exception

---
+++ src/app/RETIRODE_APP/Exceptions/CalibrationLidarException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace RETIRODE_APP.Exceptions
+{
+    public class CalibrationLidarException : Exception
+    {
+        public CalibrationLidarException()
+        {
+        }
+
+        public CalibrationLidarException(string message) : base(message)
+        {
+        }
+
+        public CalibrationLidarException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected CalibrationLidarException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
